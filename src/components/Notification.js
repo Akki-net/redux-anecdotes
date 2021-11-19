a554450376430b3abb028097da4994c0ba2bbf93
Akash Sharma <akashsharma.zak@gmail.com>
2021-11-19 22:16:26 +0530

    Displayed a msg for the duration of five seconds

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { addNotification } from '../reducers/notificationReducer'
 
 const Notification = () => {
   const notification = useSelector(state => state.notification)
+  const dispatch = useDispatch()
 
   const style = {
     border: 'solid',
@@ -11,11 +13,16 @@ const Notification = () => {
   }
   
   const showNotification = () => {
+    showMagic()
     return (
       <div style={style}>
         {notification}
       </div>
     )
+  }
+
+  const showMagic = () => {
+    setTimeout(() => dispatch(addNotification('BLA', `I don't know`)), 5000)
   }
 
   return (
