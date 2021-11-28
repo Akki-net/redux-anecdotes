127e9aa9fbf0a08fb7e8d000fb7a54f9b396f182
Akash Sharma <akashsharma.zak@gmail.com>
2021-11-28 21:29:29 +0530

    fetched the anecdotes from the backend implemented using json-server

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Notification from './components/Notification'
 import Filter from './components/Filter'
+import anecServices from './services/anecServices'
+import { intializeAnec } from './reducers/anecdoteReducer'
+import { useDispatch } from 'react-redux'
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(async () => {
+    const result = await anecServices.getAll()
+    dispatch(intializeAnec(result))
+  }, [dispatch])
+
   return (
     <div>
       <h2>Anecdotes</h2>
--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -1,12 +1,3 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
 const getId = () => (100000 * Math.random()).toFixed(0)
 
 const asObject = (anecdote) => {
@@ -17,15 +8,16 @@ const asObject = (anecdote) => {
   }
 }
 
-const initialState = anecdotesAtStart.map(asObject)
-
-const reducer = (state = initialState, action) => {
+const reducer = (state = [], action) => {
   switch(action.type){
     case 'NEW_VOTE': 
       return addVote(state, action.data.id)
 
     case 'NEW_ANEC':
       return [...state, action.data]
+
+    case 'INIT_ANEC': 
+      return action.data
   }
 
   return state
@@ -56,6 +48,13 @@ const addVote = (state = [] , id) => {
     votes : anec.votes + 1
   }
   return state.map(s => s.id === id ? obj : s)
+}
+
+export const intializeAnec = anecs => {
+  return {
+    type: 'INIT_ANEC',
+    data: anecs
+  }
 }
 
 export default reducer
